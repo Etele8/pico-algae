8c6f87aa9a753171f5b8c34f0ab25d08add95f71
Etele Kovács <etikova777@gmail.com>
2026-02-24 13:39:16 +0100

    Fixed new torch version gradscaler version importing

--- src/train/amp.py
+++ src/train/amp.py
@@ -3,5 +3,5 @@ import torch
 
 def get_scaler(use_amp: bool, device: torch.device):
     if use_amp and device.type == "cuda":
-        return  torch.amp.grad_scaler("cuda")
+        return  torch.amp.GradScaler("cuda")
     return None
